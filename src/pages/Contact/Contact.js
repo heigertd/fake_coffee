2da04f20dd720c87193805ee52fcbf68ddc6803c
Devin Heigert <heigertd@gmail.com>
2020-12-14 17:09:04 -0800

    contact styling and social icons

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,24 +8,18 @@ export default function Contact() {
             <Header navbar = {true} />
             <div className = 'contact-div'>
                 <form className = 'form-div'>
-                    <div className = 'name-form'>
-                        <div>
-                            <label for="fname">First name:</label>
-                            <input type="text" id="fname" name="fname"></input>
-                        </div>
-                        <div>
-                            <label for="lname">Last name:</label>
-                            <input type="text" id="lname" name="lname"></input>
-                        </div>
-                        <div>
-                            <label for="email">Email:</label>
-                            <input type="text" id="email" name='email'></input>
-                        </div>
-                    </div>
-                    <label for="message">Message:</label>
-                    <textarea id="message" name="message" rows='10' columns = '30'></textarea>
-                    <input type="reset" value="Submit"></input>
+                    {/* <div className = 'name-form'> */}
+                        <input type="text" id="fname" name="fname" placeholder = 'Name'></input>
+                        <input type="text" id="email" name='email' placeholder = 'Email'></input>  
+                    {/* </div> */}
+                    <textarea id="message" name="message" rows='10' columns = '30' placeholder = 'Message Goes Here!'></textarea>
+                    <input className = 'submit-button' type="reset" value="Submit"></input>
                 </form>
+                <div className = 'social-media'>
+                    <img src="https://img.icons8.com/fluent-systems-regular/56/000000/instagram-new.png"/>
+                    <img src="https://img.icons8.com/fluent-systems-regular/56/000000/facebook-new.png"/>
+                    <img src="https://img.icons8.com/android/56/000000/twitter.png"/>
+                </div>
             </div>
         </div>
     )
