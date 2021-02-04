b83b2ca62fab9a49e06f4b103491a196c1d245b0
Devin Heigert <heigertd@gmail.com>
2021-02-03 16:30:05 -0800

    menu styling

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,24 +1,57 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import menu from './menu-data'
+import menu from './menu-data';
+
+import coffee from '../../images/coffee2.jpg';
+import sandwich from '../../images/sandwich.jpg';
 import './menu.css'
 
 export default function Menu() {
     return (
+        // <div className = 'menu-page'>
+        //     <Header navbar = {true} header = 'Menu'/>
+        //         {menu.map((type) =>
+        //             <div className = 'menu-div'>
+        //                 <table className = 'menu-table'>
+        //                     {type.map((item) => 
+        //                         <tr className = 'menu-row'>
+        //                             <td>{item.item}</td>
+        //                             <td>{item.price}</td>  
+        //                         </tr>
+        //                     )}
+        //                 </table>
+        //             </div>
+        //         )}
+        // </div>
+
         <div className = 'menu-page'>
             <Header navbar = {true} header = 'Menu'/>
-                {menu.map((type) =>
-                    <div className = 'menu-div'>
-                        <table className = 'menu-table'>
-                            {type.map((item) => 
-                                <tr className = 'menu-row'>
-                                    <td>{item.item}</td>
-                                    <td>{item.price}</td>  
-                                </tr>
-                            )}
-                        </table>
-                    </div>
-                )}
+            
+            <div className = 'menu-div'>
+                <div className = 'ind-menu'>
+                    <img src = {coffee}></img>
+                    <table className = 'menu-table'>
+                        {menu[0].map((item) => 
+                            <tr className = 'menu-row'>
+                                <td>{item.item}</td>
+                                <td>{item.price}</td>  
+                            </tr>
+                        )}
+                    </table>
+                </div>
+                <div className = 'ind-menu'>
+                    <table className = 'menu-table'>
+                        {menu[1].map((item) => 
+                            <tr className = 'menu-row'>
+                                <td>{item.item}</td>
+                                <td>{item.price}</td>  
+                            </tr>
+                        )}
+                    </table>
+                    <img src = {sandwich}></img>
+                </div>
+            </div>
+                
         </div>
     )
 }
